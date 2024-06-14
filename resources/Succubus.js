17a60e73e2b29497e9c9fe23ec8ec30faef473a0
Qiong-Mengzi <2860793267@qq.com>
2024-06-14 16:26:18 +0800

    (文本匹配搜索和信息显示支持)

--- resources/Succubus.js
+++ resources/Succubus.js
@@ -58,7 +58,6 @@ var Succubus;
         return JSON.parse(JSON.stringify(obj));
     }
     Succubus.JSON_DeepCopy = JSON_DeepCopy;
-    // ! 弃用
     // ありがとう，私の暗い世界の小さな太陽
     // 复制文本到剪贴板
     // 适用于有多个相同用途的复制按钮（所以上面那个函数到底有什么用啊）
@@ -68,12 +67,63 @@ var Succubus;
             var element = elements.item(i);
             element.addEventListener('click', (() => {
                 let use_text = text[i]; // 捕获当前循环的文本值
+                let sc = success_event_curse;
+                let fc = failed_event_curse;
                 return () => {
                     navigator.clipboard.writeText(use_text)
-                        .then(() => success_event_curse(), () => failed_event_curse());
+                        .then(() => sc(), () => fc());
                 };
             })());
         }
     }
     Succubus.ClipboardCopyByClassT = ClipboardCopyByClassT;
+    function Searching(text, target, format) {
+        var t_text = JSON_DeepCopy(text);
+        var t_text_score = new Array(text.length);
+        for (var i = 0; i < text.length; i++) {
+            t_text_score[i] = format(t_text[i], target);
+        }
+        // Sorting
+        // 之后想到什么算法再写罢...（如果谁会快速排序能否帮忙写一下w)
+        // I don't know how to quickly sort.
+        // If anyone knows, could you help me write it down? Thank you.
+        for (var j = 0; j < text.length - 1; j++) {
+            for (var i = 0; i < text.length - j - 1; i++) {
+                if (t_text_score[i] > t_text_score[i + 1]) {
+                    // Swap scores
+                    let sort_t_num = t_text_score[i];
+                    t_text_score[i] = t_text_score[i + 1];
+                    t_text_score[i + 1] = sort_t_num;
+                    // Swap Text
+                    let sort_t_text = t_text[i];
+                    t_text[i] = t_text[i + 1];
+                    t_text[i + 1] = sort_t_text;
+                }
+            }
+        }
+        return [t_text, t_text_score];
+    }
+    Succubus.Searching = Searching;
+    function BaseTextMatchCurse(text, target) {
+        let reg = RegExp('[' + target + ']+', 'ig');
+        let result = text.match(reg);
+        if (result == null)
+            return -1;
+        let max_length = 0;
+        let great_of_middle_length = 0;
+        let match_num = result.length;
+        let length_table = result.map(x => x.length);
+        for (let i = 0; i < length_table.length; i++) {
+            let lobj = length_table[i];
+            if (lobj > max_length)
+                max_length = lobj;
+        }
+        for (let i = 0; i < length_table.length; i++) {
+            let lobj = length_table[i];
+            if (lobj > max_length / 2)
+                great_of_middle_length += 1;
+        }
+        return (max_length * 0.9 + match_num * 0.7 / text.length + great_of_middle_length * 0.4 / text.length) / 2;
+    }
+    Succubus.BaseTextMatchCurse = BaseTextMatchCurse;
 })(Succubus || (Succubus = {}));
--- resources/Succubus.ts
+++ resources/Succubus.ts
@@ -50,20 +50,74 @@ namespace Succubus {
     // ありがとう，私の暗い世界の小さな太陽
     // 复制文本到剪贴板
     // 适用于有多个相同用途的复制按钮（所以上面那个函数到底有什么用啊）
-    export function ClipboardCopyByClassT(class_id: string, text: Array<string>, success_event_curse: CallableFunction = () => {}, failed_event_curse: CallableFunction = () => { console.log('Clipboard Write Failed.') }) {
+    export function ClipboardCopyByClassT(class_id: string, text: Array<string>, success_event_curse: CallableFunction = () => { }, failed_event_curse: CallableFunction = () => { console.log('Clipboard Write Failed.') }) {
         var elements = document.getElementsByClassName(class_id);
         for (var i = 0; i < elements.length; i++) {
             var element = elements.item(i) as HTMLElement;
-            element.addEventListener('click', (() => {
+            element.addEventListener('click', ((): any => {
                 let use_text = text[i]; // 捕获当前循环的文本值
+                let sc = success_event_curse
+                let fc = failed_event_curse
                 return () => {
                     navigator.clipboard.writeText(use_text)
                         .then(
-                            () => success_event_curse(),
-                            () => failed_event_curse()
+                            () => sc(),
+                            () => fc()
                         );
                 }
             })());
         }
+    }
+
+    // Searching
+    interface SearchingFormat {
+        (text: string, target: string): number
+    }
+    export function Searching(text: Array<string>, target: string, format: SearchingFormat) {
+        var t_text: Array<string> = JSON_DeepCopy(text)
+        var t_text_score: Array<number> = new Array(text.length)
+        for (var i = 0; i < text.length; i++) {
+            t_text_score[i] = format(t_text[i], target)
+        }
+        // Sorting
+        // 之后想到什么算法再写罢...（如果谁会快速排序能否帮忙写一下w)
+        // I don't know how to quickly sort.
+        // If anyone knows, could you help me write it down? Thank you.
+        for (var j = 0; j < text.length - 1; j++) {
+            for (var i = 0; i < text.length - j - 1; i++) {
+                if (t_text_score[i] > t_text_score[i + 1]) {
+                    // Swap scores
+                    let sort_t_num = t_text_score[i]
+                    t_text_score[i] = t_text_score[i + 1]
+                    t_text_score[i + 1] = sort_t_num
+                    // Swap Text
+                    let sort_t_text = t_text[i]
+                    t_text[i] = t_text[i + 1]
+                    t_text[i + 1] = sort_t_text
+                }
+            }
+        }
+        return [t_text, t_text_score]
+    }
+    export function BaseTextMatchCurse(text: string, target: string) {
+        let reg = RegExp('[' + target + ']+', 'ig')
+        let result = text.match(reg)
+        if (result == null)
+            return -1
+        let max_length = 0
+        let great_of_middle_length = 0
+        let match_num = result.length
+        let length_table = result.map(x => x.length)
+        for (let i = 0; i < length_table.length; i++) {
+            let lobj = length_table[i]
+            if (lobj > max_length)
+                max_length = lobj
+        }
+        for (let i = 0; i < length_table.length; i++) {
+            let lobj = length_table[i]
+            if (lobj > max_length / 2)
+                great_of_middle_length += 1
+        }
+        return (max_length * 0.9 + match_num * 0.7 / text.length + great_of_middle_length * 0.4 / text.length) / 2
     }
 }
